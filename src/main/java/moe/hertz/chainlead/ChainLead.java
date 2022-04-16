2b36dc9ae4650ff106dff1d5925a9b7814ee8c36
codehz <codehz@outlook.com>
2022-04-16 15:01:18 +0800

    initial commit

---
+++ src/main/java/moe/hertz/chainlead/ChainLead.java
@@ -0,0 +1,34 @@
+package moe.hertz.chainlead;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.LeadItem;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.Box;
+
+public class ChainLead implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            if (player.isSpectator() || !player.isSneaking() || world.isClient) return ActionResult.PASS;
+            var stack = player.getStackInHand(hand);
+            if (stack.getItem() instanceof LeadItem && entity instanceof MobEntity mob) {
+                var pos = mob.getPos();
+                var min = pos.add(-7, -7, -7);
+                var max = pos.add(7, 7, 7);
+                player.sendMessage(new LiteralText("Use lead"), false);
+                var entities = world.getNonSpectatingEntities(MobEntity.class, new Box(min, max));
+                var handled = false;
+                for (var selected : entities) {
+                    if (selected.getHoldingEntity() != player) continue;
+                    selected.attachLeash(mob, true);
+                    handled = true;
+                }
+                if (handled) return ActionResult.SUCCESS;
+            }
+            return ActionResult.PASS;
+        });
+    }
+}
